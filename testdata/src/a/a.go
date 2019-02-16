44bfed0a4e4673c7cb17d5149787734e197b1d62
Takumasa Sakao <sakataku7@gmail.com>
2019-02-16 19:08:26 +0900

    Prepare testing with xerrros

--- testdata/src/a/a.go
+++ testdata/src/a/a.go
@@ -1,5 +1,9 @@
 package a
 
+import "golang.org/x/xerrors"
+
+var sentinelErr = xerrors.New("sentinel err")
+
 func main() {
-	// want "pattern"
+	xerrors.Errorf("hogehoge: %w", sentinelErr)
 }
---
+++ testdata/src/a/go.mod
@@ -0,0 +1,3 @@
+module a
+
+require golang.org/x/xerrors v0.0.0-20190212162355-a5947ffaace3
