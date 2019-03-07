7128234dea3048669c9882152346a2dc26c9850e
Matts966 <USER_INFO@users.noreply.github.com>
2019-03-08 01:54:32 +0900

    deal with switch statements (WIP)

--- passes/erreq/erreq.go
+++ passes/erreq/erreq.go
@@ -2,6 +2,7 @@ package erreq
 
 import (
 	"go/ast"
+	"go/token"
 	"go/types"
 
 	"golang.org/x/tools/go/analysis"
@@ -11,24 +12,43 @@ import (
 
 var Analyzer = &analysis.Analyzer{
 	Name: "erreq",
-	Doc:  Doc,
+	Doc:  "erreq finds binary ops and switch-case statements comparing errors",
 	Run:  run,
 	Requires: []*analysis.Analyzer{
 		inspect.Analyzer,
 	},
 }
 
-const Doc = "erreq finds binary expression which is comparing error"
-
 var errType = types.Universe.Lookup("error").Type().Underlying().(*types.Interface)
+
+var causeFuncs = []string{
+	"TestCauseFunc", // For test
+
+	"github.com/pkg/errors.Cause",
+}
 
 func run(pass *analysis.Pass) (interface{}, error) {
 	inspect := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
-	nodeFilter := []ast.Node{(*ast.BinaryExpr)(nil)}
 
+	checkBinaryOps(pass, inspect)
+
+	checkSwitchStmt(pass, inspect)
+
+	return nil, nil
+}
+
+// Check boolean expression using == or !=.
+func checkBinaryOps(pass *analysis.Pass, inspect *inspector.Inspector) {
+	nodeFilter := []ast.Node{
+		(*ast.BinaryExpr)(nil),
+	}
 	inspect.Preorder(nodeFilter, func(node ast.Node) {
 		binExpr, ok := node.(*ast.BinaryExpr)
 		if !ok {
+			return
+		}
+
+		if !(binExpr.Op == token.EQL || binExpr.Op == token.NEQ) {
 			return
 		}
 
@@ -50,7 +70,56 @@ func run(pass *analysis.Pass) (interface{}, error) {
 			return
 		}
 
-		pass.Reportf(node.Pos(), "do not compare error with \"==\" or \"!=\"")
+		pass.Reportf(node.Pos(), "do not compare errors with binary ops.")
 	})
-	return nil, nil
+}
+
+// Check switch-case statement.
+func checkSwitchStmt(pass *analysis.Pass, inspect *inspector.Inspector) {
+	nodeFilter := []ast.Node{
+		(*ast.SwitchStmt)(nil),
+	}
+	inspect.Preorder(nodeFilter, func(node ast.Node) {
+
+		switchStmt, ok := node.(*ast.SwitchStmt)
+
+		if !ok {
+			return
+		}
+
+		tagType, ok := pass.TypesInfo.Types[switchStmt.Tag]
+		if !ok {
+			return
+		}
+
+		if !types.AssignableTo(tagType.Type, errType) {
+			return
+		}
+
+		// if the tag of switch stmt is wrapped by Cause func, it should be allowed.
+		callExp, ok := switchStmt.Tag.(*ast.CallExpr)
+		if ok {
+			if expIsCauseFunc(callExp.Fun) {
+				return
+			}
+		}
+
+		pass.Reportf(node.Pos(), "a")
+	})
+}
+
+func expIsCauseFunc(exp ast.Expr) bool {
+	funIdent, ok := exp.(*ast.Ident)
+
+	if !ok {
+		return false
+	}
+
+	for _, funcName := range causeFuncs {
+		if funIdent.Name == funcName {
+			return true
+		}
+	}
+
+	return false
 }
--- passes/erreq/testdata/src/a/a.go
+++ passes/erreq/testdata/src/a/a.go
@@ -6,11 +6,33 @@ var sentinelErr = fmt.Errorf("sentinel err")
 
 func main() {
 	_ = alwaysErr() == nil         // OK
-	_ = alwaysErr() == sentinelErr // want `do not compare error with "==" or "!="`
+	_ = alwaysErr() == sentinelErr // want `do not compare errors with binary ops.`
 	_ = alwaysErr() != nil         // OK
-	_ = alwaysErr() != sentinelErr // want `do not compare error with "==" or "!="`
+	_ = alwaysErr() != sentinelErr // want `do not compare errors with binary ops.`
 }
 
 func alwaysErr() error {
 	return sentinelErr
+}
+
+func switchErrCase1(err error) string {
+	switch err { // want `a`
+	case sentinelErr:
+		return "true"
+	default:
+		return "false"
+	}
+}
+
+func switchErrCase2(err error) string {
+	switch TestCauseFunc(err) { // OK
+	case sentinelErr:
+		return "true"
+	default:
+		return "false"
+	}
+}
+
+func TestCauseFunc(err error) error {
+	return err
 }
